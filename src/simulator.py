e5a3343c36b5efaf87212b8f635a6e02fa92d1ec
Filip Keerberg <filip.keerberg@gmail.com>
2026-04-20 20:03:27 +0200

    Added types and basic endpoints

--- src/simulator.py
+++ src/simulator.py
@@ -1,8 +1,51 @@
 from state import State
+from agent import *
+
+from copy import deepcopy
 
 class Simulator:
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int):
         self.width = width
         self.height = height
-        self.agents = []
+        self.agents: list[Agent] = []
         self.ground_truth = State(width, height)
+
+
+
+    # Adds an agent to the simulation
+    def add_agent(self, agent: Agent):
+        self.agents.append(agent)
+        self.ground_truth.agents.matrix[agent.y][agent.x] = 1
+
+
+
+    # Performs a single step of the simulation.
+    # This includes agent actions, environment actions, and updating the ground truth.
+    # Returns the ground truth state after the step.
+    def step(self):
+        # Perform agent actions
+        for agent in self.agents:
+            action = agent.get_action()
+            is_move = action < 4
+            
+            if is_move:
+                self.ground_truth.agents.matrix[agent.y][agent.x] = 0
+                agent.move(action)
+                self.ground_truth.agents.matrix[agent.y][agent.x] = 1
+            
+            elif action == AgentAction.SCAN:
+                agent.scan(self.ground_truth)
+
+        # Perform environment actions (firespread, etc.)
+
+        # Don't let the returned state modify current state
+        result = deepcopy(self.ground_truth)
+        return result 
+
+    # Performs a number of steps of the simulation.
+    # Returns a list of the ground truth states after each step.
+    def run(self, steps: int):
+        record: list[State] = []
+        for _ in range(steps):
+            record.append(self.step())
+        return record
