604447568ed18fd7ff6c435ecf5c521ed30181fc
Jacob Dimša <j.dimsa-1@student.tudelft.nl>
2026-04-27 03:23:25 +0200

    enum to have a state of the simulation

--- src/metric.py
+++ src/metric.py
@@ -1,16 +1,12 @@
+from enum import Enum
 from .agent import Agent
 
-class Metric():
-    """
-    What metrics do we even want here?
 
-    per agent or overall? - maria & tigo?
+class RunOutcome(Enum):
+    IN_PROGRESS = "in_progress"
+    SUCCESS = "success"
+    TIMEOUT = "timeout"
 
-    "bad" metrics: terrain collisions, victims run over, agent collisions, fire?
-    "good" metrics: victims found, what else?
-    """
 
-    terrain_collisions: dict[Agent, int]
-    victim_collisions: dict[Agent, int]
-    inter_agent_collisions: int
-    victims_found: int
+class Metric:
+    pass
