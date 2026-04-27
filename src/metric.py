fa832ef15a9fab6c0fd209df6db77be52d3f94b7
Jacob Dimša <j.dimsa-1@student.tudelft.nl>
2026-04-27 03:27:30 +0200

    added collisions, wait count and damage metrics

--- src/metric.py
+++ src/metric.py
@@ -9,4 +9,39 @@ class RunOutcome(Enum):
 
 
 class Metric:
-    pass
+    def __init__(self):
+        self.terrain_collisions: dict[Agent, int] = {} # Number of times agent hits a wall
+        self.victim_collisions: dict[Agent, int] = {} # Number of times it hits a victim
+        self.inter_agent_collisions: dict[Agent, int] = {} # Number of times it collides with another agent
+        self.wait_actions: dict[Agent, int] = {}  # How many times the agent chooses to wait
+        self.damage: dict[Agent, int] = {}  # (terrain collisions + victim collisions + inter-agent collisions) 
+                                            # * damage per collision (for now all same)
+
+        self.outcome: RunOutcome = RunOutcome.IN_PROGRESS
+        self.steps_taken: int = 0
+        self.victims_found: int = 0
+        self.total_victims: int = 0
+        self.time_to_first_found: int | None = None
+        self.time_to_all_found: int | None = None
+
+    def register_agent(self, agent: Agent) -> None:
+        self.terrain_collisions[agent] = 0
+        self.victim_collisions[agent] = 0
+        self.inter_agent_collisions[agent] = 0
+        self.wait_actions[agent] = 0
+        self.damage[agent] = 0
+
+    def record_terrain_collision(self, agent: Agent) -> None:
+        self.terrain_collisions[agent] += 1
+        self.damage[agent] += 1
+
+    def record_victim_collision(self, agent: Agent) -> None:
+        self.victim_collisions[agent] += 1
+        self.damage[agent] += 1
+
+    def record_inter_agent_collision(self, agent: Agent) -> None:
+        self.inter_agent_collisions[agent] += 1
+        self.damage[agent] += 1
+
+    def record_wait(self, agent: Agent) -> None:
+        self.wait_actions[agent] += 1
