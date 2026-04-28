5cccc65b821555560da1539f8e9bd40ad5db5335
Tigo Schutgens <t.r.schutgens@student.tudelft.nl>
2026-04-28 16:44:55 +0200

    fixed failing test

--- test/agent_test.py
+++ test/agent_test.py
@@ -55,8 +55,8 @@ class TestAgentMove(unittest.TestCase):
     def test_agent_trajectory(self):
         agnt = Agent("x", x=1, y=1, width=10, height=10,
                      scan_accuracy=0.05, scan_radius=0, sigma=0.9)
-        self.assertEqual(agnt.trajectory, [(1, 1)])
+        self.assertEqual(agnt.move_history, [(1, 1)])
 
         agnt.move(AgentAction.MOVE_UP)
         agnt.move(AgentAction.MOVE_RIGHT)
-        self.assertEqual(agnt.trajectory, [(1, 1), (1, 0), (2, 0)])
+        self.assertEqual(agnt.move_history, [(1, 1), (1, 0), (2, 0)])
