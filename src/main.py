33d9292d427744aa067a8f76cd0ad3a6170eb339
fartdemarco <89381788+fartdemarco@users.noreply.github.com>
2026-04-28 15:26:15 +0200

    fix(cleanup main comments)

--- src/main.py
+++ src/main.py
@@ -6,24 +6,18 @@ WIDTH = 40
 HEIGHT = 25
 
 def main():
-    # 1. Setup the world
     sim = Simulator(WIDTH, HEIGHT)
     config = MapConfig(num_rooms=5, num_victims=5)
     sim.generate_ground_truth(config)
 
    #  visualize_grid_gen(sim.ground_truth.traversability, sim.ground_truth.agents, sim.ground_truth.victims)
 
-    # 2. Setup Bob (The Agent)
-    # x=12, y=12 is roughly center. sigma=0.01, accuracy=0.9, radius=4
     bob = Agent("Bob", 12, 12, WIDTH, HEIGHT, 0.01, 0.9, 4)
     sim.add_agent(bob)
 
-    # 3. Run the simulation
-    # This will now trigger bob.scan() and bob.move() inside simulator.step()
     print("Running Simulation steps...")
     history = sim.run(30) 
 
-    # 4. Visualize the result
     print("Launching Visualizer...")
     viz = Visualizer(history, cell_size=25)
     viz.run()
