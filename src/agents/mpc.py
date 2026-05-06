4d3b9047dbd605ab89e8a5ae83750c08699e1cb6
Filip Keerberg <filip.keerberg@gmail.com>
2026-05-06 11:16:07 +0200

    Changed predictions to use models

--- src/agents/mpc.py
+++ src/agents/mpc.py
@@ -5,6 +5,12 @@ from src.agent import Agent, AgentAction
 from src.state import State
 from copy import deepcopy
 
+class Model:
+    def __init__(self, state: State, x: int, y: int):
+        self.state = state
+        self.agent_x = x
+        self.agent_y = y
+
 class MpcAgent(Agent):
     def __init__(self, name: str, x: int, y: int, width: int, height: int, 
                  decay: float, scan_accuracy: float, scan_radius: int):
@@ -27,18 +33,18 @@ class MpcAgent(Agent):
         best_objective = float('-inf')
 
         for sequence in itertools.product(AgentAction, repeat=self.horizon):
-            state = deepcopy(self.perception)
+            model = Model(deepcopy(self.perception), self.x, self.y)
 
             total_objective = 0.0
             feasible = True
 
             for step, action in enumerate(sequence):
-                if not self._is_feasible(state, action):
+                if not self._is_feasible(model, action):
                     feasible = False
                     break
 
-                state = self._predict_next_state(state, action)
-                total_objective += (self.gamma ** step) * self._objective(state)
+                model = self._predict_next_state(model, action)
+                total_objective += (self.gamma ** step) * self._objective(model)
             
             if feasible and total_objective > best_objective:
                 best_sequence = sequence
@@ -46,52 +52,56 @@ class MpcAgent(Agent):
 
         return best_sequence[0]
 
-    def _objective(self, state: State) -> float:
+    def _objective(self, model: Model) -> float:
         """
-        Calculates the objective value of a given state
+        Calculates the objective value of a given model
 
         Parameters:
-        state: The state to calculate the objective for
+        model: The model to calculate the objective for
 
         Returns:
         The objective value
         """
         w1, w2, w3, w4 = 1, 1, 1, 1 # To be adjusted
 
-        victim_term =  w1 * self._victim_score(state)
-        exploration =  w2 * self._exploration_score(state)
-        safety      = -w3 * self._safety_penalty(state)
-        confidence  =  w4 * self._confidence_score(state)
+        victim_term =  w1 * self._victim_score(model)
+        exploration =  w2 * self._exploration_score(model)
+        safety      = -w3 * self._safety_penalty(model)
+        confidence  =  w4 * self._confidence_score(model)
 
         return victim_term + exploration + safety + confidence
 
-    def _predict_next_state(self, state: State, action: AgentAction) -> State:
+    def _predict_next_state(self, model: Model, action: AgentAction) -> Model:
         """
         Predicts what the next state will look like if the agent performs the given action.
         This assumes the action is feasible (see self._is_feasible)
 
         Parameters:
-        state: The current state of the world
+        model: The current model of the world
         action: The action to perform
 
         Returns:
-        The predicted next state
+        The predicted next model
         """
-        new_state = deepcopy(state)
+        new_model = deepcopy(model)
 
         match action:
             case AgentAction.MOVE_UP:
-                new_state.agents[self.y - 1][self.x] = 1
-                new_state.agents[self.y][self.x] = 0
+                new_model.state.agents[self.y - 1][self.x] = 1
+                new_model.state.agents[self.y][self.x] = 0
+                new_model.y -= 1
             case AgentAction.MOVE_DOWN:
-                new_state.agents[self.y + 1][self.x] = 1
-                new_state.agents[self.y][self.x] = 0
+                new_model.state.agents[self.y + 1][self.x] = 1
+                new_model.state.agents[self.y][self.x] = 0
+                new_model.y += 1
             case AgentAction.MOVE_LEFT:
-                new_state.agents[self.y][self.x - 1] = 1
-                new_state.agents[self.y][self.x] = 0
+                new_model.state.agents[self.y][self.x - 1] = 1
+                new_model.state.agents[self.y][self.x] = 0
+                new_model.x -= 1
             case AgentAction.MOVE_RIGHT:
-                new_state.agents[self.y][self.x + 1] = 1
-                new_state.agents[self.y][self.x] = 0
+                new_model.state.agents[self.y][self.x + 1] = 1
+                new_model.state.agents[self.y][self.x] = 0
+                new_model.x += 1
 
         # TODO: Spread fire.
         # ? What approach?
@@ -102,76 +112,78 @@ class MpcAgent(Agent):
         
         # TODO: Mock scanning (updating confidences, tiles explored), currently scanning would scan from the old position; might not be necessary
 
-        return new_state
+        return new_model
 
-    def _is_feasible(self, state: State, action: AgentAction) -> bool:
+    def _is_feasible(self, model: Model, action: AgentAction) -> bool:
         """
-        Decides whether a given action will result in a feasible state
+        Decides whether a given action will result in a feasible position
         (e.g. not walking into a wall)
 
         Parameters:
-        state: The perceived state of the world
+        model: The perceived model of the world
         action: The action to check
 
         Returns:
         Whether the action is feasible
         """
-        target_cell_x, target_cell_y = self._target_cell(action)
+        target_cell_x, target_cell_y = self._target_cell(model.agent_x, model.agent_y, action)
         if (target_cell_x < 0 or target_cell_x >= self.world_width or 
             target_cell_y < 0 or target_cell_y >= self.world_height): # Out of bounds
             return False
         
-        return state.traversability[target_cell_y][target_cell_x] == 0 # Didn't hit wall
+        return model.state.traversability[target_cell_y][target_cell_x] == 0 # Didn't hit wall
 
 
-    def _target_cell(self, action: AgentAction) -> tuple[int, int]:
+    def _target_cell(self, x: int, y: int, action: AgentAction) -> tuple[int, int]:
         """
         Calculates the cell targeted by the agent given its action.
 
         Parameters:
+        x: Starting x position of the agent
+        y: Starting y position of the agent
         action: The action the agent wants to perform
 
         Returns:
         The target cell indices
         """
-        tx, ty = self.x, self.y
         match action:
             case AgentAction.MOVE_UP:
-                ty -= 1
+                return x, y - 1
             case AgentAction.MOVE_DOWN:
-                ty += 1
+                return x, y + 1
             case AgentAction.MOVE_LEFT:
-                tx -= 1
+                return x - 1, y
             case AgentAction.MOVE_RIGHT:
-                tx += 1
-        return tx, ty
+                return x + 1, y
+        
+        return x, y # Wait action
     
-    def _victim_score(self, state: State) -> float:
+    def _victim_score(self, model: Model) -> float:
         """
-        Calculates a score of a theoretical state in regards to the number of victims found.
+        Calculates a score of a model in regards to the number of victims found.
 
         Parameters:
-        state: The state to calculate the score for
+        model: The model to calculate the score for
 
         Returns:
         The score
         """
-        # ? A predicted state should never have more victims
+        # ? A predicted state should never have found more victims
         # ? so this should be equal for all predicted states;
         # ? is there a point in having this?
-        return np.sum(state.victims.matrix) 
+        return np.sum(model.state.victims.matrix) 
     
-    def _exploration_score(self, state: State) -> float:
+    def _exploration_score(self, model: Model) -> float:
         """
-        Calculates a score of a theoretical state in regards to how much area is explored.
+        Calculates a score of a model in regards to how much area is explored.
 
         Parameters:
-        state: The state to calculate the score for
+        model: The model to calculate the score for
 
         Returns:
         The score
         """
-        explored = np.count_nonzero(state.confidence.matrix)
+        explored = np.count_nonzero(model.state.confidence.matrix)
         return explored
         # Find agent position in this predicted state
         
--- test/test_mpcagent.py
+++ test/test_mpcagent.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.agents.mpc import MpcAgent
+from src.agents.mpc import MpcAgent, Model
 from src.constants import AgentAction
 from src.state import State
 
@@ -11,30 +11,32 @@ class MpcAgentTest(unittest.TestCase):
         ground_truth = State(2, 2)
         ground_truth.traversability[0][1] = 1
         agent.scan(ground_truth)
-        self.assertFalse(agent._is_feasible(agent.perception, AgentAction.MOVE_RIGHT))
+        self.assertFalse(agent._is_feasible(Model(agent.perception, agent.x, agent.y), AgentAction.MOVE_RIGHT))
 
     def test_is_feasible_oob(self):
         agent = MpcAgent("mpc", 0, 0, 1, 1, 0.1, 0.9, 1)
-        self.assertFalse(agent._is_feasible(agent.perception, AgentAction.MOVE_RIGHT))
-        self.assertFalse(agent._is_feasible(agent.perception, AgentAction.MOVE_UP))
-        self.assertFalse(agent._is_feasible(agent.perception, AgentAction.MOVE_LEFT))
-        self.assertFalse(agent._is_feasible(agent.perception, AgentAction.MOVE_DOWN))
+        model = Model(agent.perception, agent.x, agent.y)
+        self.assertFalse(agent._is_feasible(model, AgentAction.MOVE_RIGHT))
+        self.assertFalse(agent._is_feasible(model, AgentAction.MOVE_UP))
+        self.assertFalse(agent._is_feasible(model, AgentAction.MOVE_LEFT))
+        self.assertFalse(agent._is_feasible(model, AgentAction.MOVE_DOWN))
 
     def test_is_feasible_valid(self):
         agent = MpcAgent("mpc", 0, 0, 2, 2, 0.1, 0.9, 1)
         ground_truth = State(2, 2)
         ground_truth.traversability[0][1] = 1
         agent.scan(ground_truth)
-        self.assertTrue(agent._is_feasible(agent.perception, AgentAction.MOVE_DOWN))
-        self.assertTrue(agent._is_feasible(agent.perception, AgentAction.WAIT))
+        model = Model(agent.perception, agent.x, agent.y)
+        self.assertTrue(agent._is_feasible(model, AgentAction.MOVE_DOWN))
+        self.assertTrue(agent._is_feasible(model, AgentAction.WAIT))
 
     def test_target_cell_move(self):
         agent = MpcAgent("mpc", 1, 1, 3, 3, 0.1, 0.9, 1)
-        self.assertEqual(agent._target_cell(AgentAction.MOVE_UP), (1, 0))
-        self.assertEqual(agent._target_cell(AgentAction.MOVE_DOWN), (1, 2))
-        self.assertEqual(agent._target_cell(AgentAction.MOVE_LEFT), (0, 1))
-        self.assertEqual(agent._target_cell(AgentAction.MOVE_RIGHT), (2, 1))
+        self.assertEqual(agent._target_cell(agent.x, agent.y, AgentAction.MOVE_UP), (1, 0))
+        self.assertEqual(agent._target_cell(agent.x, agent.y, AgentAction.MOVE_DOWN), (1, 2))
+        self.assertEqual(agent._target_cell(agent.x, agent.y, AgentAction.MOVE_LEFT), (0, 1))
+        self.assertEqual(agent._target_cell(agent.x, agent.y, AgentAction.MOVE_RIGHT), (2, 1))
 
     def test_target_cell_wait(self):
         agent = MpcAgent("mpc", 1, 1, 3, 3, 0.1, 0.9, 1)
-        self.assertEqual(agent._target_cell(AgentAction.WAIT), (1, 1))
+        self.assertEqual(agent._target_cell(agent.x, agent.y, AgentAction.WAIT), (1, 1))
